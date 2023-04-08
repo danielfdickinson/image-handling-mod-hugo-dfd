f99386902db7a602f0e3a6197b6aabf371781a26
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-04-08 13:40:40 -0400

    Allow deploy preview absolute links to work
    Deploy previews absolute links need to point to the deploy preview, not the
    production site.
    
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/netlify_build.sh
+++ scripts/netlify_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 if [ "$CONTEXT" = "production" ]; then
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
 else
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$DEPLOY_PRIME_URL"
 fi
 
-HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $URL --source exampleSite --destination $(pwd)/public --config config.toml
+HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL --source exampleSite --destination "$(pwd)/public" --config config.toml
