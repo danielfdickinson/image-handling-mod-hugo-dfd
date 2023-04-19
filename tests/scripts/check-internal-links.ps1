1d06778e0d2ed44980ccb3f702aaedcda606266c
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-04-19 12:09:34 -0400

    Tweak check-internal-links script for Windows
    It still complains about the LICENSE file, but at least only that one.
    
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/check-internal-links.ps1
+++ tests/scripts/check-internal-links.ps1
@@ -1,3 +1,3 @@
 cd exampleSite\public
-npx.cmd hyperlink index.html --root . --canonicalroot https://image-handling-mod-ci.demo.wildtechgarden.com/ --internal --todo "301 http" --todo "fragment-redirect" -r --verbose -p index.html
+npx.cmd hyperlink index.html --root . --canonicalroot https://image-handling-mod-ci.demo.wildtechgarden.com/ --internal --todo "301 http" --todo "fragment-redirect" -c 10 -r index.html | npx.cmd tap-spot
 cd ..\..
