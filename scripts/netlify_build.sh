a04258613f61f8d9b0f8bafc8ecf810f31c3c88d
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-04-18 22:00:37 -0400

    Fix paths for netlify build
    We need to make sure the paths are in fact correct and that checklinks
    uses the right root path and base url.
    
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/netlify_build.sh
+++ scripts/netlify_build.sh
@@ -3,10 +3,18 @@
 set -e
 set -o pipefail
 
+pip install pre-commit
+
+pre-commit install --install-hooks
+pre-commit run --all-files
+
+bash ./tests/scripts/hugo-audit.sh
+rm -rf public exampleSite/public
+
 if [ "$CONTEXT" = "production" ]; then
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
 else
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$DEPLOY_PRIME_URL"
 fi
 
-HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL --source exampleSite --destination "$(pwd)/public" --config config.toml
+HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL --source exampleSite --destination "$(pwd)/public" --config "$(pwd)/exampleSite/config.toml"
