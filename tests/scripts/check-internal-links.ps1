02edb552a049e198756aeb217abdc29ba6b6b35d
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-04-27 14:29:24 -0400

    Move CI site to ci.thecshore.com as primary domain
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/check-internal-links.ps1
+++ tests/scripts/check-internal-links.ps1
@@ -1,3 +1,3 @@
 cd exampleSite\public
-npx.cmd hyperlink index.html --root . --canonicalroot https://image-handling-mod-ci.demo.wildtechgarden.com/ --internal --todo "301 http" --todo "fragment-redirect" -c 10 -r index.html | npx.cmd tap-spot
+npx.cmd hyperlink index.html --root . --canonicalroot https://image-handling-mod.ci.thecshore.com/ --internal --todo "301 http" --todo "fragment-redirect" -c 10 -r index.html | npx.cmd tap-spot
 cd ..\..
