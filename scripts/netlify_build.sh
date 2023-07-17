aca616a65cc1062bd0d71c7ed54699146f25d29d
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-07-17 14:42:05 -0400

    Add workaround for Go issue with LFS in modules
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/netlify_build.sh
+++ scripts/netlify_build.sh
@@ -3,13 +3,24 @@
 set -e
 set -o pipefail
 
-pip install pre-commit
-
-pre-commit install --install-hooks
-pre-commit run --all-files
+# Ensure Go modules with LFS checksum correctly
+# Workaround for https://github.com/golang/go/issues/41708
+# We don't have LFS on this repo
+# git lfs install
 
 bash ./tests/scripts/hugo-audit.sh
+
 rm -rf public exampleSite/public
+
+export HUGO_PARAMS_DEFAULTCANONICAL="$URL"
+
+if [ "$CONTEXT" = "production" ]; then
+	export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
+	export BASEURL="$URL"
+else
+	export HUGO_PARAMS_DEPLOYEDBASEURL="$DEPLOY_PRIME_URL"
+	export BASEURL="$DEPLOY_PRIME_URL"
+fi
 
 if [ "$CONTEXT" = "production" ]; then
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
@@ -17,4 +28,4 @@ else
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$DEPLOY_PRIME_URL"
 fi
 
-HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL --source exampleSite --destination "$(pwd)/public" --config "$(pwd)/exampleSite/config.toml"
+HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL --source "$(pwd)"/exampleSite --destination "$(pwd)"/public
